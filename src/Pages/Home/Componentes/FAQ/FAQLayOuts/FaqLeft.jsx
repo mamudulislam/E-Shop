7070015e8838aab4b506aa588cfddb2eb9ea54ec
Mamudul_islam <mamuduli153@gmail.com>
2025-05-31 17:51:01 +0600

    collaps add ant desingn

--- src/Pages/Home/Componentes/FAQ/FAQLayOuts/FaqLeft.jsx
+++ src/Pages/Home/Componentes/FAQ/FAQLayOuts/FaqLeft.jsx
@@ -1,8 +1,22 @@
+import { Collapse } from 'antd'
 import React from 'react'
 
 const FaqLeft = () => {
     return (
-        <div>FaqLeft</div>
+        <div>
+            <Collapse
+                items={[{
+                    key: '1',
+                    className: "Custom-collapse",
+                    label: 'This is default size panel header',
+                    children:
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Voluptas sit ullam necessitatibus iusto asperiores totam ipsum eligendi soluta,
+                            nobis neque, repellendus quaerat?
+                        </p>
+                }]}
+            />
+        </div>
     )
 }
 
