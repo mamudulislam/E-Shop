31e65c2d18c17c5844eb63f67897ea30a2a0ae22
Mamudul_islam <mamuduli153@gmail.com>
2025-06-21 15:38:44 +0600

    contact pages done

--- src/Pages/Contact/Componentes/ContactBody.jsx
+++ src/Pages/Contact/Componentes/ContactBody.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ContactForm from './ContactForm'
 import { Link, useLocation } from 'react-router-dom'
+import { FaPhoneAlt } from 'react-icons/fa';
+import { IoIosMail } from 'react-icons/io';
+import { FaLocationDot } from 'react-icons/fa6';
 
 const ContactBody = () => {
     const { pathname } = useLocation();
@@ -18,8 +21,44 @@ const ContactBody = () => {
                 </h1>
                 <ContactForm />
             </div>
-            <div>bdr</div>
-        </div>
+            <div>
+                <div className='bg-white02 p-10 mt-40 rounded-[25px]'>
+                    <h3 className='font-Poppins font-semibold text-[24px] text-black01 pb-4'>Let’s Keep in Touch!</h3>
+                    <p className='font-Montserrat font-normal text-[18px] leading-[30px] text-black100'>We would love to hear from you. Contact us for
+                        any inquiries you might have for us.</p>
+                    <div className='mt-10'>
+                        <div>
+                            <ul>
+                                <li className='flex items-center gap-x-2 font-Montserrat font-normal text-black01 mb-3'>
+                                    <FaPhoneAlt color='#828282' />
+                                    <span>+1 (555) 123-4567</span>
+                                </li>
+                                <li className='flex items-center gap-x-2 font-Montserrat font-normal text-black01 mb-3'>
+                                    <IoIosMail color='#828282' />
+                                    <span>information@eshop.com</span>
+                                </li>
+                                <li className='flex items-center gap-x-2 font-Montserrat font-normal text-black01'>
+                                    <FaLocationDot color='#828282' />
+                                    <span>123 Main Street, Suite 105, Anytown USA</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='mt-10'>
+                        <h4 className='font-Montserrat font-bold text-[20px]'>Opening Hours</h4>
+                        <div className='border border-b-3 border-orange w-[159px]'></div>
+                    </div>
+                    <div className='mt-6 gap-y-2'>
+                        <div className='flex gap-x-3'>
+                            <h4 className='font-Montserrat font-bold text-[16px] text-black01'>MON to FRI:</h4><span className='font-Montserrat font-normal text-[16px] text-black01'>08:00 AM - 04:00 PM</span>
+                        </div>
+                        <div className='flex gap-x-3'>
+                            <h4 className='font-Montserrat font-bold text-[16px] text-black01'>SAT to SUN:</h4><span className='font-Montserrat font-normal text-[16px] text-black01'>09:00 AM - 03:00 PM</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
--- src/Pages/Contact/Componentes/ContactForm.jsx
+++ src/Pages/Contact/Componentes/ContactForm.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const ContactForm = () => {
     return (
-        <div>ContactForm</div>
+        <>
+            <div>
+                <h2 className='font-Poppins font-semibold text-4xl text-black01'>Contact Us</h2>
+                <p className='font-Montserrat text-xl text-black01 font-normal mt-3 mb-[48px]'>Have any questions for us? Don’t hesitate to contact us.</p>
+                <form>
+                    <div>
+                        <label className='block font-Montserrat text-xl font-bold text-black01 mb-2'>Name <span className='text-orange'>*</span></label>
+                        <input type="text" placeholder='Mamudul Islam' className='w-full border border-black100 p-5 rounded-[10px] outline-none' />
+                    </div>
+                    <div className='grid grid-cols-2 gap-x-4 my-8'>
+                        <div>
+                            <label className='block font-Montserrat text-xl font-bold text-black01 mb-2'>Phone Number <span className='text-orange'>*</span></label>
+                            <input type="text" placeholder='+8801234567890' className='w-full border border-black100 p-5 rounded-[10px] outline-none' />
+                        </div>
+                        <div>
+                            <label className='block font-Montserrat text-xl font-bold text-black01 mb-2'>Email Address <span className='text-orange'>*</span></label>
+                            <input type="email" placeholder='mamudulislam@gmail.com' className='w-full border border-black100 p-5 rounded-[10px] outline-none' />
+                        </div>
+                    </div>
+                    <div>
+                        <label className='block font-Montserrat text-xl font-bold text-black01 mb-2'>Message<span className='text-orange'>*</span></label>
+                        <textarea placeholder='message ...' className='w-full border border-black100 p-5 rounded-[10px] outline-none resize-none h-[241px]' />
+                    </div>
+                    <button
+                        type="submit"
+                        className="!bg-orange !px-10 !h-12 !border-none !mt-[16px] rounded-[10px] cursor-pointer"
+                    >
+                        <span className="font-montserrat font-bold text-lg text-white">
+                            Submit
+                        </span>
+                    </button>
+                </form>
+            </div>
+        </>
     )
 }
 
