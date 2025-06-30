4ef93c7bfcffdaefcd17feaf345fcdf2efc67aaa
Mamudul_islam <mamuduli153@gmail.com>
2025-06-30 13:15:34 +0600

    check error in pagination delete

--- src/Pages/Product/Components/Rightproductcomponent.jsx
+++ src/Pages/Product/Components/Rightproductcomponent.jsx
@@ -3,17 +3,11 @@ import Showecsebody from './Productshowecase/Showecsebody'
 import ShowcaseHeader from './Productshowecase/ShowcaseHeader'
 
 const Rightproductcomponent = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pageSize, setPageSize] = useState(16);
     return (
         <>
             <div>
-                <ShowcaseHeader currentPage={currentPage}
-                    pageSize={pageSize} />
-                <Showecsebody currentPage={currentPage}
-                    setCurrentPage={setCurrentPage}
-                    pageSize={pageSize}
-                    setPageSize={setPageSize} />
+                <ShowcaseHeader />
+                <Showecsebody />
             </div>
         </>
     )
